5ad83ab82eda50e9d076df1cebf78f98b271b5de
agubelu <agustinbd3@gmail.com>
2023-12-01 22:27:16 +0100

    💀 Day 01 💀

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,41 +1,37 @@
 use crate::{Solution, SolutionPair};
 use itertools::*;
+use regex::Regex;
 use std::fs::read_to_string;
-use SearchOrder::*;
+
 ///////////////////////////////////////////////////////////////////////////////
 
-enum SearchOrder { First, Last }
+static OPTIONS: [&str; 10] = ["\\d", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
 pub fn solve() -> SolutionPair {
     let input = read_to_string("input/day01.txt").unwrap();
     let lines = input.lines().collect_vec();
 
-    let sol1: u32 = lines.iter().copied().map(line_value).sum();
-    let sol2: u32 = lines.iter().map(|line| line_value(&transform_letters(line))).sum();
+    let re1_start = build_regex(&OPTIONS[..1], "");
+    let re1_end = build_regex(&OPTIONS[..1], ".*");
+    let re2_start = build_regex(&OPTIONS, "");
+    let re2_end = build_regex(&OPTIONS, ".*");
+
+    let sol1: usize = lines.iter().map(|line| line_value(line, &re1_start, &re1_end)).sum();
+    let sol2: usize = lines.iter().map(|line| line_value(line, &re2_start, &re2_end)).sum();
 
     (Solution::from(sol1), Solution::from(sol2))
 }
 
-fn transform_letters(line: &str) -> String {
-    let transforms = [("one", "o1e"), ("two", "t2o"), ("three", "t3e"),
-                      ("four", "f4r"), ("five", "f5e"), ("six", "s6x"),
-                      ("seven", "s7n"), ("eight", "e8t"), ("nine", "n9e")];
-
-    let mut res = line.to_owned();
-    for (from, to) in transforms {
-        res = res.replace(from, to);
-    }
-
-    res
+fn line_value(line: &str, re_start: &Regex, re_end: &Regex) -> usize {
+    let start_val = re_start.captures(line).unwrap().get(1).unwrap().as_str();
+    let end_val = re_end.captures(line).unwrap().get(1).unwrap().as_str();
+    substr_to_number(start_val) * 10 + substr_to_number(end_val)
 }
 
-fn line_value(line: &str) -> u32 {
-    find_number(line, First) * 10 + find_number(line, Last)
+fn substr_to_number(s: &str) -> usize {
+    s.parse().unwrap_or_else(|_| OPTIONS.iter().position(|n| *n == s).unwrap())
 }
 
-fn find_number(line: &str, order: SearchOrder) -> u32 {
-    match order {
-        First => line.chars().find_map(|ch| ch.to_digit(10)).unwrap(),
-        Last => line.chars().rev().find_map(|ch| ch.to_digit(10)).unwrap()
-    }
+fn build_regex(options: &[&str], prefix: &str) -> Regex {
+    Regex::new(&format!("{prefix}({})", options.join("|"))).unwrap()
 }
